bbbc8823941ad96f34188fcb96d7de78fd93d396
Ian_TORNIER <ian.tornier@isen-ouest.yncrea.fr>
2025-06-12 15:24:18 +0200

    createfile

--- createfile.c
+++ createfile.c
@@ -1,12 +1,19 @@
 #include "createfile.h"
 
 void createFiles(struct Text* text, int chapters_number){
-    for (int i = 0; i < 10; i++ ) {
-        printf("Test");
+    for (int i = 1; i <= text->chapter_number; i++) { // on commence au chapitre 1
+        char file_name[64];
+        snprintf(file_name, 64, "%d.html", i);
+
+        FILE *f = fopen(file_name, "w");
+        if ( f == NULL) {
+            printf("Erreur de lecture du chapitre : %d\n", i);
+            exit(1);
+        }
+        
+        const char* chapter_in_text = (const char*)text->chapters[i-1];
+        fprintf(f, "%s", chapter_in_text);
+        fclose(f);
     }
-
-
-
-
 }
 
