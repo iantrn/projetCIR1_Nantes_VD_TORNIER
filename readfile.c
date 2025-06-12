e08df0395373eba82996b7ddb0e4b6f5e6a882ce
Nicolas vandonselaar <nicolas.van-donselaar@isen-ouest.yncrea.fr>
2025-06-12 13:54:19 +0200

    vrai readfile

--- readfile.c
+++ readfile.c
@@ -1,20 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "readfile.h"
 #define LINE_SIZE 512
 
-const char* filename = "book.txt";
 
-int main(void) {
+struct Chapter {
+    char chapter_title[256];
+    int chapter_id;
+    char text[256];
+    char choices;
+
+};
+
+
+
+const char filename = "book.txt";
+
+void readFile(char* filename) {
     FILE* file = fopen(filename, "r");
     if (file == NULL) {
         printf("Unable to open file <%s>\n", filename);
         exit(EXIT_FAILURE);
     }
-
     char line[LINE_SIZE];
-    while(fgets(line, sizeof(line), file)) {
-    // ...
+    while (fgets(line, sizeof(line), file)) {
+
     }
+
 }
+
+
 
 
 
