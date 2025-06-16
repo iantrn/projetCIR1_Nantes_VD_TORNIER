6615feed68c29a4a6f7fbeb821c942973c758c95
Ian_TORNIER <ian.tornier@isen-ouest.yncrea.fr>
2025-06-16 09:50:16 +0200

    beke

--- createfile.c
+++ createfile.c
@@ -17,7 +17,7 @@ void createFiles(struct Text* text, int chapter_id){
     }
     fprintf(f, "<!DOCTYPE html>\n<html>\n<head><meta charset=\"UTF-8\">\n<title>%s</title>\n<link rel=\"%s\" href=\"%s\">\n<script src=\"%s\" defer></script>\n</head>\n<body>\n",  text->chapters[chapter_id - 1].title, stylesheet, css_file, js_file);
     fprintf(f, "<h1>%s</h1>\n", text->chapters[chapter_id - 1].title);
-    fprintf(f, "%s\n",text->chapters[chapter_id - 1].content );
+    fprintf(f, "<div class=\"content\">\n%s\n</div>\n",text->chapters[chapter_id - 1].content );
     for (int i = 0; i < MAX_CHOICES; i++) {
         int chapter_choice_id = text->chapters[chapter_id - 1].choices[i].choices_id;
         if ( chapter_choice_id > 0) {
--- web/main.js
+++ web/main.js
@@ -1,26 +1,6 @@
 function main() {
-    function choices_animations() {
-        const fade = document.getElementById('fade');
-        const choicesDiv = document.querySelector('.choices');
-
-        choicesDiv.addEventListener('click', (event) => {
-        const target = event.target;
-        if (target.tagName === 'A') {
-            event.preventDefault();
-
-            const url = target.href;
-
-            fade.style.pointerEvents = 'auto';
-            fade.style.opacity = '1';
-
-            setTimeout(() => {
-            window.location.href = url;
-            }, 1000);
-        }
-    });
-  }
+  
     let choices_animation = document.getElementsByClassName("choices");
-
 
 
 }
