343b2a1fe4a05b42fc40c02a7c05a4930c401561
Ian_TORNIER <ian.tornier@isen-ouest.yncrea.fr>
2025-06-16 11:02:42 +0200

    audio

--- createfile.c
+++ createfile.c
@@ -27,7 +27,7 @@ void createFiles(struct Text* text, int chapter_id){
         continue;
         }
     fprintf(f, "<div id=\"overlay\"></div>\n");
-    fprintf(f,"<audio id=\"background-audio\" src=\"../web/images/background_music.mp3\" loop></audio>");
+    fprintf(f,"<audio id=\"background-audio\" src=\"../web/images/background_musique.mp3\" loop></audio>");
     fprintf(f, "</body>\n</html>\n");
         
     fclose(f);
