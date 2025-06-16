5c6437e08914ab6dd2acd1aef2a0bbad9f80ec44
Ian_TORNIER <ian.tornier@isen-ouest.yncrea.fr>
2025-06-16 10:18:09 +0200

    css

--- web/main.js
+++ web/main.js
@@ -1,11 +1,21 @@
 function main() {
-  
-    let choices_animation = document.getElementsByClassName("choices");
+const links = document.querySelectorAll('.choice');
+  const overlay = document.getElementById('overlay');
 
+  links.forEach(link => {
+        link.addEventListener('click', function(event) {
+      event.preventDefault(); // bloque la navigation immédiate
 
+      // Affiche le fond noir en opacity 1 (fade-in)
+      overlay.classList.add('active');
+
+      // Après la fin de la transition (ici 500ms), on navigue vers le href
+      setTimeout(() => {
+        window.location.href = link.href;
+      }, 3000);
+    });
+  });
 }
-
-
 
 
 
